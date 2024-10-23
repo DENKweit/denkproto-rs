bf2d50690186318b2a7a6e5b5e2cc0fe471f0d72
Azure Pipelines <accounts@denkweit.de>
2024-10-23 11:21:18 +0000

    updated to version 1.0.19

--- build.rs
+++ build.rs
@@ -4,10 +4,10 @@ fn main() {
         // Use `protoc` parser, optional.
         .protoc()
         // All inputs and imports from the inputs must reside in `includes` directories.
-        .includes(&["../proto"])
+        .includes(&["./proto"])
         // Inputs must reside in some of include paths.
-        .input("../proto/DENKbuffer.proto")
-        .input("../proto/modelfile-v2.proto")
+        .input("./proto/DENKbuffer.proto")
+        .input("./proto/modelfile-v2.proto")
         // Specify output directory relative to Cargo output directory.
         .out_dir("src/gen")
         .run_from_script();
