9f5858fef76b358f7f69a631cdf89370ce877daf
Azure Pipelines <accounts@denkweit.de>
2025-04-08 11:41:38 +0000

    updated to version 1.0.61

--- build.rs
+++ build.rs
@@ -1,19 +1,12 @@
 // Use this in build.rs
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    protobuf_codegen::Codegen::new()
-        // Use `protoc` parser, optional.
-        .protoc()
-        // All inputs and imports from the inputs must reside in `includes` directories.
-        .includes(&["./proto"])
-        // Inputs must reside in some of include paths.
-        .input("./proto/DENKbuffer.proto")
-        .input("./proto/modelfile-v2.proto")
-        .input("./proto/denkcache.proto")
-        .input("./proto/inference_graph.proto")
-        // Specify output directory relative to Cargo output directory.
-        .cargo_out_dir("protos")
-        .run_from_script();
-
-    tonic_build::compile_protos("proto/denkcache.proto")?;
+    prost_validate_build::Builder::new().compile_protos(
+        &[
+            "./proto/inference_graph.proto",
+            "./proto/modelfile-v2.proto",
+            "./proto/denkcache.proto",
+        ],
+        &["./proto"],
+    )?;
     Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,15 @@
-include!(concat!(env!("OUT_DIR"), "/protos/mod.rs"));
+// include!(concat!(env!("OUT_DIR"), "/protos/mod.rs"));
+
+pub mod modelfile {
+    pub mod v2 {
+        include!(concat!(env!("OUT_DIR"), "/modelfile.v2.rs"));
+    }
+}
+
+pub mod denkcache {
+    include!(concat!(env!("OUT_DIR"), "/denkcache.rs"));
+}
 
 pub mod denkcache_proto {
-    tonic::include_proto!("denkcache"); // The string specified here must match the proto package name
+    include!(concat!(env!("OUT_DIR"), "/inference_graph.rs"));
 }
